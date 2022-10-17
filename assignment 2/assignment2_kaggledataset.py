3e2c50963c86cf04a8ab216ec7622562ac5368a5
ArishMada <arishmadataly@gmail.com>
2022-10-17 14:41:43 +0700

    updating

--- assignment 2/assignment2_kaggledataset.py
+++ assignment 2/assignment2_kaggledataset.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
@@ -36,7 +37,7 @@ kidneyData["classification"] = kidneyData["classification"].replace(to_replace=[
 # cleaning the rows that have empty cells
 kidneyData = kidneyData.dropna()
 
-x = kidneyData.drop("classification", axis=1)
+x = kidneyData.drop("classification", axis=1).loc[:, ["age", "hemo"]]
 y = kidneyData['classification'].astype("int")
 
 # print(microbeDataSample["microorganisms"].unique())
@@ -49,12 +50,52 @@ clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
 print(f'Accuracy:{metrics.accuracy_score(y_test, y_pred) * 100}%')
 
+
+# plt.scatter(kidneyData["age"][kidneyData["classification"] == 1], kidneyData["hemo"][kidneyData["classification"] == 1],
+#             color='red', marker='o', label='kidney disease')
+# plt.scatter(kidneyData["age"][kidneyData["classification"] == 0], kidneyData["hemo"][kidneyData["classification"] == 0],
+#             color='blue', marker='*', label='no kidney disease')
+# plt.xlim()
+# plt.xlabel('age')
+# plt.ylabel("hemoglobin")
+# plt.legend(loc='upper left')
+# plt.show()
+
+# plotting with surface color
+def make_meshgrid(x, y, h=.02):
+    x_min, x_max = x.min() - 1, x.max() + 1
+    y_min, y_max = y.min() - 1, y.max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    return xx, yy
+
+
+def plot_contours(ax, clf, xx, yy, **params):
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    out = ax.contourf(xx, yy, Z, **params)
+    return out
+
+
+fig, ax = plt.subplots()
+# title for the plots
+title = 'Kidney disease classification'
+# Set-up grid for plotting.
+X0, X1 = x.loc[:, "age"], x.loc[:, "hemo"]
+xx, yy = make_meshgrid(X0, X1)
+
+plot_contours(ax, clf, xx, yy, cmap="Set1", alpha=0.8)
+
 plt.scatter(kidneyData["age"][kidneyData["classification"] == 1], kidneyData["hemo"][kidneyData["classification"] == 1],
-            color='red', marker='o', label='kidney disease')
+            color='black', marker='o', label='kidney disease')
 plt.scatter(kidneyData["age"][kidneyData["classification"] == 0], kidneyData["hemo"][kidneyData["classification"] == 0],
             color='blue', marker='*', label='no kidney disease')
+
 plt.xlim()
-plt.xlabel('age')
-plt.ylabel("hemoglobin")
-plt.legend(loc='upper left')
+ax.set_ylabel('hemoglobin number')
+ax.set_xlabel('age')
+plt.locator_params('x', nbins=20)
+ax.locator_params('y', nbins=10)
+print(max(kidneyData["hemo"]))
+ax.set_title(title)
+ax.legend()
 plt.show()
--- assignment 2/assignment2_toydataset.py
+++ assignment 2/assignment2_toydataset.py
@@ -5,16 +5,15 @@ from sklearn import metrics
 import matplotlib.pyplot as plt
 import pandas as pd
 
+import numpy as np
+
 # Load dataset
 from sklearn.preprocessing import StandardScaler
 
 iris = datasets.load_iris()
 df = pd.DataFrame(data=iris.data)
 df["label"] = iris.target
-# print(iris.target)
-# for i in range(149):
-#     if df.loc
-print(iris.data)
+
 # print the names of the 13 features
 print("Features: ", iris.feature_names)
 
@@ -32,14 +31,54 @@ y_pred = clf.predict(X_test)
 # Model Accuracy: how often is the classifier correct?
 print(f'Accuracy:{round(metrics.accuracy_score(y_test, y_pred) * 100, 2)}%')
 
-# plotting
+
+# plotting in 2D without surface color
+# plt.scatter(df[0][df["label"] == 0], df[1][df["label"] == 0],
+#             color='red', marker='o', label='setosa')
+# plt.scatter(df[0][df["label"] == 1], df[1][df["label"] == 1],
+#             color='blue', marker='*', label='versicolor')
+# plt.scatter(df[0][df["label"] == 2], df[1][df["label"] == 2],
+#             color='black', marker='^', label='virginica')
+# plt.xlabel('sepal length')
+# plt.ylabel('sepal width')
+# plt.legend(loc='upper left')
+# plt.show()
+
+# plotting with surface color
+def make_meshgrid(x, y, h=.02):
+    x_min, x_max = x.min() - 1, x.max() + 1
+    y_min, y_max = y.min() - 1, y.max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    return xx, yy
+
+
+def plot_contours(ax, clf, xx, yy, **params):
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    out = ax.contourf(xx, yy, Z, **params)
+    return out
+
+
+fig, ax = plt.subplots()
+# title for the plots
+title = ('Types of iris based on sepal length and width')
+# Set-up grid for plotting.
+X0, X1 = iris.data[:, 0], iris.data[:, 1]
+xx, yy = make_meshgrid(X0, X1)
+
+plot_contours(ax, clf, xx, yy, cmap=plt.cm.coolwarm, alpha=0.8)
+
 plt.scatter(df[0][df["label"] == 0], df[1][df["label"] == 0],
-            color='red', marker='o', label='setosa')
+            color='white', marker='o', label='setosa')
 plt.scatter(df[0][df["label"] == 1], df[1][df["label"] == 1],
             color='blue', marker='*', label='versicolor')
 plt.scatter(df[0][df["label"] == 2], df[1][df["label"] == 2],
             color='black', marker='^', label='virginica')
-plt.xlabel('sepal length')
-plt.ylabel('sepal width')
-plt.legend(loc='upper left')
+
+ax.set_ylabel('sepal length(cm)')
+ax.set_xlabel('sepal width(cm)')
+plt.locator_params('x', nbins=20)
+ax.locator_params('y', nbins=10)
+ax.set_title(title)
+ax.legend()
 plt.show()
